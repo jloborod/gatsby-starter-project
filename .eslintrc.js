30c98fc91152b120cf14cc60a52317e6c9246500
Joaquín Lobo Rodríguez <jloborod@gmail.com>
2019-02-15 09:59:25 +0000

    created basic project structure

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  plugins: ['react'],
+  globals: {
+    graphql: false,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+    },
+  },
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Gatsby Starter Project
+
+Simple Gatsby repo with the main features you might need to build your static website.
+
+To run locally, `yarn`, then `yarn dev`, then open https://localhost:8000.
